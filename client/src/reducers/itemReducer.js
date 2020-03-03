94fa2e79e8b5f7658a19b0745446cadc59c118e2
Soloman Garcia <soloman.garcia@gmail.com>
2020-03-03 13:59:58 -0700

    add itemReducer.js

---
+++ client/src/reducers/itemReducer.js
@@ -0,0 +1,10 @@
+import uuidv1 from 'uuid/v1';
+
+const initalState = {
+    items: [
+        { id: uuidv1(), name: 'Eggs' },
+        { id: uuidv1(), name: 'Milk' },
+        { id: uuidv1(), name: 'Steak' },
+        { id: uuidv1(), name: 'Water' }
+    ]
+}
