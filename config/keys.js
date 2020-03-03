ad86c00b5f71e2e7250273ddfc4ca9eaa279c599
Soloman Garcia <soloman.garcia@gmail.com>
2020-03-03 09:36:43 -0700

    create and connect DB

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://sgarci81:!Vanilla1@cluster0-1bkj8.mongodb.net/test?retryWrites=true&w=majority'
+}
--- server.js
+++ server.js
@@ -6,3 +6,16 @@ const app = express();
 
 // BodyParser Middleware
 app.use(bodyParser.json());
+
+// DB config
+const db = require('./config/keys').mongoURI;
+
+// Connect to Mongo
+mongoose
+    .connect(db)
+    .then(() => console.log('MongoDB connected...'))
+    .catch(err => console.log('err'));
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
