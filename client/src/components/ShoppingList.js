ce17f7254c7853939c9a3c2bc32d9a8174756871
Soloman Garcia <soloman.garcia@gmail.com>
2020-03-03 17:02:36 -0700

    degug npm package redux-thunk

--- client/src/components/ShoppingList.js
+++ client/src/components/ShoppingList.js
@@ -2,19 +2,18 @@ import React, { Component } from 'react';
 import { Container, ListGroup, ListGroupItem, Button } from 'reactstrap';
 import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import uuidv1 from 'uuid/v1';
+import { connect } from 'react-redux';
+import { getItems } from '../actions/itemActions';
+import PropTypes from 'prop-types';
 
 class ShoppingList extends Component {
-    state = {
-        items: [
-            { id: uuidv1(), name: 'Eggs' },
-            { id: uuidv1(), name: 'Milk' },
-            { id: uuidv1(), name: 'Steak' },
-            { id: uuidv1(), name: 'Water' }
-        ]
+
+    componentDidMount() {
+        this.props.getItems();
     }
 
     render() {
-        const { items } = this.state;
+        const { items } = this.props.item;
         return(
             <Container>
                 <Button
@@ -59,4 +58,13 @@ class ShoppingList extends Component {
     }
 }
 
-export default ShoppingList;
+ShoppingList.propTypes ={
+    getItems: PropTypes.func.isRequired,
+    item: PropTypes.object.isRequired
+}
+
+const mapStateToProps = (state) => ({
+    item: state.item
+});
+
+export default connect(mapStateToProps, { getItems })(ShoppingList);
